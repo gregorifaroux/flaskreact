e5f1866fa2a9b02f37487e9cdd15d93a4eb2728f
Gregori Faroux <gregorifaroux@Catalytic-GF.local>
2018-07-27 16:41:37 -0400

    Clean template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,27 +6,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 class="content-subhead">API Call Example</h1>
         <p className="App-intro">
           <form method="POST" action="/findcommonletters" name="webapp" class="pure-form pure-form-aligned">
 
             <fieldset>
-              <legend>Python</legend>
               <div class="pure-control-group">
                   <label for="sentence">Sentence:</label>
-                  <input id="sentence" type="text" size="80"  placeholder="A sentence"></input>
+                  <input id="sentence" type="text" size="40"  placeholder="A sentence"></input>
               </div>
               <div class="pure-control-group">
                   <label for="letters">Letters:</label>
                   <input id="letters" type="text" size="10"  placeholder="Enter letters"></input>
               </div>
 
-              <button type="submit" class="pure-button">
-                  Common letters
-              </button>
-
-              <button type="submit" formaction="/countvowels" class="pure-button">
-                Count vowels
-              </button>
+              <div class="pure-control-group">
+                <button type="submit" class="pure-button">
+                    Common letters
+                </button>
+                <button type="submit" formaction="/countvowels" class="pure-button">
+                  Count vowels
+                </button>
+              </div>
             </fieldset>
 
 
