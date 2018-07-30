9d133584c33f44c57ccdc21ca25fe4a969ce0329
Gregori Faroux <gregorifaroux@Catalytic-GF.local>
2018-07-30 17:11:32 -0400

    add call to API

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 import backend.utils as myutils
+
 
 
 @app.route('/')
@@ -10,8 +13,9 @@ def home() -> 'html':
 
 @app.route('/findcommonletters', methods=['POST'])
 def findcommonletters() -> 'html':
-  results = myutils.find_common_letters(request.form['sentence'], request.form['letters'])
-  return render_template('results.html', results_title='Common Letters:', the_results = results)
+#  results = myutils.find_common_letters(request.form['sentence'], request.form['letters'])
+#  return render_template('results.html', results_title='Common Letters:', the_results = results)
+	return '{ "name":"Fake data" }'
 
 @app.route('/countvowels', methods=['POST'])
 def countvowels() -> 'html':
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    
+    fetch('http://127.0.0.1:5000/findcommonletters', {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+    })
+    .then(response => response.json())
+    .then(data =>  this.setState({
+      res: JSON.stringify(data)
+    })).catch(err => {
+      console.error('Error: ', err)
+    });
+  }
+
+  
+
   render() {
+    const { res } = this.state;
     return (
       <div className="App">
-        <h1 class="content-subhead">API Call Example</h1>
-        <p className="App-intro">
-          <form method="POST" action="/findcommonletters" name="webapp" class="pure-form pure-form-aligned">
+        <h1 className="content-subhead">API Call Example</h1>
+        <div className="App-intro">
+          <form onSubmit={this.handleSubmit} name="webapp" className="pure-form pure-form-aligned">
 
             <fieldset>
-              <div class="pure-control-group">
-                  <label for="sentence">Sentence:</label>
-                  <input id="sentence" type="text" size="40"  placeholder="A sentence"></input>
+              <div className="pure-control-group">
+                  <label htmlFor="sentence">Sentence:</label>
+                  <input id="sentence" name="sentence" type="text" size="40"  placeholder="A sentence" required></input>
               </div>
-              <div class="pure-control-group">
-                  <label for="letters">Letters:</label>
-                  <input id="letters" type="text" size="10"  placeholder="Enter letters"></input>
+              <div className="pure-control-group">
+                  <label htmlFor="letters">Letters:</label>
+                  <input id="letters" name="letters" type="text" size="10"  placeholder="Enter letters" required></input>
               </div>
 
-              <div class="pure-control-group">
-                <button type="submit" class="pure-button">
+              <div className="pure-control-group">
+                <button className="pure-button">
                     Common letters
                 </button>
-                <button type="submit" formaction="/countvowels" class="pure-button">
+                <button className="pure-button">
                   Count vowels
                 </button>
               </div>
@@ -33,7 +60,13 @@ class App extends Component {
 
 
       </form>        
-</p>
+      </div>
+      <div className="res-block">
+          	<div>
+              <h3>Results:</h3>
+              <pre>FormData {res}</pre>
+          	</div>
+        </div>
       </div>
           );
         }
